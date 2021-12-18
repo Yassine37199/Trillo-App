2b32f588bb363dfdf7101d0661b70a11213b637d
Yassine37199 <yassine.ouri7319@gmail.com>
2021-12-18 20:12:09 +0100

    Built Overall Layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './styles/main.scss';
 function App() {
   return (
     <div className="App">
-      
+       <div className='container'>
+         <header className='header'>
+           Header Part
+         </header>
+         <div className='content'>
+           <nav className='sidebar'>
+             Navigation
+           </nav>
+           <main className='hotel-view'>
+             Hotel View
+           </main>
+         </div>
+       </div>
     </div>
   );
 }
