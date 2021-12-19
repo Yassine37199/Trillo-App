ce5f39d9e9a76f4ac6fc785d895384831cb81b58
Yassine37199 <yassine.ouri7319@gmail.com>
2021-12-19 12:39:31 +0100

    Styling header icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import logo from './img/logo.png';
+import UserPhoto from './img/user-4.jpg';
+
+import {ReactComponent as GlassIcon} from './img/SVG/magnifying-glass.svg';
+import {ReactComponent as BookmarkIcon} from './img/SVG/bookmark.svg';
+import {ReactComponent as ChatIcon} from './img/SVG/chat.svg';
+
 import './styles/main.scss';
 
 function App() {
@@ -6,7 +12,27 @@ function App() {
     <div className="App">
        <div className='container'>
          <header className='header'>
-           Header Part
+           <img src={logo} alt="trillo logo" className="logo"/>
+           <form action='#' className='search'>
+             <input type="text" className='search__input' placeholder='Search hotels'/>
+             <button className='search__button'>
+               <GlassIcon className='search__icon' />
+             </button>
+           </form>
+           <nav className='user-nav'>
+             <div className='user-nav__icon-box'>
+               <BookmarkIcon className='user-nav__icon' />
+               <span className='user-nav__notification'>7</span>
+             </div>
+             <div className='user-nav__icon-box'>
+               <ChatIcon className='user-nav__icon' />
+               <span className='user-nav__notification'>13</span>
+             </div>
+             <div className='user-nav__user'>
+               <img src={UserPhoto} className='user-nav__user-photo'/>
+               <span className='user-nav__user-name'>Yassine</span>
+             </div>
+           </nav>
          </header>
          <div className='content'>
            <nav className='sidebar'>
