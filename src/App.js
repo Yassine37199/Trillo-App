f2a10feeac8d22b87cc8629ab39262d2bc674411
Yassine37199 <yassine.ouri7319@gmail.com>
2022-02-01 19:18:32 +0100

    Completed Sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import UserPhoto from './img/user-4.jpg';
 import {ReactComponent as GlassIcon} from './img/SVG/magnifying-glass.svg';
 import {ReactComponent as BookmarkIcon} from './img/SVG/bookmark.svg';
 import {ReactComponent as ChatIcon} from './img/SVG/chat.svg';
+import {ReactComponent as HomeIcon} from './img/SVG/home.svg';
+import {ReactComponent as PlaneIcon} from './img/SVG/aircraft.svg';
+import {ReactComponent as KeyIcon} from './img/SVG/key.svg';
+import {ReactComponent as MapIcon} from './img/SVG/map.svg';
 
 import './styles/main.scss';
 
@@ -36,7 +40,40 @@ function App() {
          </header>
          <div className='content'>
            <nav className='sidebar'>
-             Navigation
+             <ul className='side-nav'>
+                <li className='side-nav__item side-nav__item--active'>
+                  <a href="#" className='side-nav__link'>
+                    <HomeIcon className="side-nav__icon" />
+                    <span>Hotel</span>
+                  </a>
+                </li>
+
+                <li className='side-nav__item'>
+                  <a href="#" className='side-nav__link'>
+                    <PlaneIcon className="side-nav__icon" />
+                    <span>Flight</span>
+                  </a>
+                </li>
+
+                <li className='side-nav__item'>
+                  <a href="#" className='side-nav__link'>
+                    <KeyIcon className="side-nav__icon" />
+                    <span>Car rental</span>
+                  </a>
+                </li>
+
+                <li className='side-nav__item'>
+                  <a href="#" className='side-nav__link'>
+                    <MapIcon className="side-nav__icon" />
+                    <span>Tours</span>
+                  </a>
+                </li>
+             </ul>
+
+
+             <div className='legal'>
+               &copy; 2017 by trillo, All rights reserved
+             </div>
            </nav>
            <main className='hotel-view'>
              Hotel View
